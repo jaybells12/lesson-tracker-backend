d4d1f119a156dbe9690300e99da26b818f3cc98b
jaybells12 <109028985+jaybells12@users.noreply.github.com>
2023-03-08 10:33:43 -0800

    Add password hashing to user schema
    Created a couple pre hooks for validating and hashing password before
    saving it to DB. Also created a schema method to compare document
    stored password to a provided value.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,12 @@
-import { Schema, model } from 'mongoose';
-import { IUser, Role } from '../interfaces/Model-Interfaces'
+import mongoose, { Schema, model, Model } from 'mongoose';
+import { IUser, IUserMethods, Role } from '../interfaces/Model-Interfaces'
+import bcrypt from "bcrypt";
 
-const userSchema = new Schema<IUser>({
+const SALT_WORK_FACTOR = 10;
+
+type UserModel = Model<IUser, {}, IUserMethods>
+
+const userSchema = new Schema<IUser, UserModel, IUserMethods>({
   username: {
     type: String,
     cast: false,
@@ -10,7 +15,6 @@ const userSchema = new Schema<IUser>({
     minLength: [3, "{PATH} is too short. Minimum length is 3 characters."],
     maxLength: [16, "{PATH} is too long. Maximum length is 16 characters."],
   },
-  // need to implement password hashing
   password: {
     type: String,
     cast: false,
@@ -30,6 +34,57 @@ const userSchema = new Schema<IUser>({
     enum: Role,
     required: [true, "Missing required field."]
   }
-},  { timestamps: true })
+}, { timestamps: true })
 
-export const User = model<IUser>("User", userSchema);
+
+//Callback accepts two parameters, either error object or null & boolean value
+userSchema.methods.comparePassword = function(candidatePassword, callback){
+  bcrypt.compare(candidatePassword, this.password, function(err, results){
+    if(err) return callback(err);
+    callback(null, results);
+  })
+}
+
+//Password Hashing with Length Validation Middleware (Save)
+userSchema.pre('save', function(next){
+  const user = this;
+
+  if(!user.isModified('password')) return next();
+
+  if(user.password.length < 10) {
+    return next(new mongoose.Error.ValidatorError({
+      message: "Password length is too short. Minimum length is 10 characters.",
+      path: 'password',
+      reason: 'Invalid length'
+    }))
+  }
+
+  bcrypt.hash(user.password, SALT_WORK_FACTOR, function(err, hash){
+    if(err) return next(err)
+    user.password = hash;
+    next()
+  })
+})
+
+//Password Hashing with Length Validation Middleware (Update)
+userSchema.pre('findOneAndUpdate', function(next) {
+  const updateObj = this.getUpdate();
+
+  if(!updateObj || !('password' in updateObj)) return next()
+
+  if(updateObj.password.length < 10) {
+    return next(new mongoose.Error.ValidatorError({
+      message: "Password length is too short. Minimum length is 10 characters.",
+      path: 'password',
+      reason: 'Invalid length'
+    }))
+  }
+
+  bcrypt.hash(updateObj.password, SALT_WORK_FACTOR, function(err, hash){
+    if(err) return next(err)
+    updateObj.password = hash;
+    next();
+  })
+})
+
+export const User = model<IUser, UserModel>("User", userSchema);
